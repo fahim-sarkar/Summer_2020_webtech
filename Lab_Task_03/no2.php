658fc26fde34ac0f5092751c7e5c4fd49cbfd312
fahim-sarkar <fahim_sarkar@hotmail.com>
2020-07-23 23:19:48 +0600

    update
    no2

--- Lab_Task_03/no2.php
+++ Lab_Task_03/no2.php
@@ -1,33 +1,58 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #00FF00}
+</style>
+</head>
+<body> 
+
+
 <?php
+$emailErr = "";
+$email =  "";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-	$name = $_POST['sname'];
-
-	if($name == "")
-	{
-		echo "Cannot be empty";
-	}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	
+	function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
-
-
+	
+	
+	
+	  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+	
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid Email Format";
+    }
+  }
+  
+}
 
 ?>
 
 
+<h2>E-mail</h2>
 
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title> Email Form</title>
-</head>
-<body>
-
-<form method="POST">
-	<b>Email:</b><br>
-	<input type="text" name="sname" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Use two Letters at least" value=""><br> <p></p>
-<input type="submit" name="" value="submit">
+ E-mail:<br/> <input type="text" name="email" placeholder="Hint: sample@example.com">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  
+  <input type="submit" name="submit" value="Submit">  
 </form>
+
+
+
+
+
+
 </body>
 </html>
